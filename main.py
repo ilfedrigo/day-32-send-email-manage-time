5189693a8881788e34d7418413caf7dcb2989b3c
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-04 11:42:10 -0300

    Final implementation of Birthday Wisher

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-# Send email on birthday
+import pandas
+import random
+import smtplib
+from datetime import datetime
 
-# Check if today is a birthday
-    # Read .csv file and check the day
+my_email = "ilfedrigo.python.100days@gmail.com"
+password = "ioqr rpwy jwdu xzig"
 
-# Pick a random letter
-    # Change the [NAME] to the actual name
-        # Take the name from the .csv file
+today = (datetime.now().month, datetime.now().day)
 
-# Send the email
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+                            from_addr=my_email, 
+                            to_addrs=birthday_person["email"], 
+                            msg=f"Subject:Happy birthday!\n\n{contents}"
+                            )
