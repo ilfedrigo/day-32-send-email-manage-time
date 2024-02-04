86447b682d9e81ef95338a3eb512f9acc2331b79
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-04 10:23:24 -0300

    Sending automated emails
    Get a random quote from a 102 quotes in a .txt file then sending quis quote to my email

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-# import smtplib
-
-# my_email = "ilfedrigo.python.100days@gmail.com"
-# password = "ioqr rpwy jwdu xzig"
-
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#                         from_addr=my_email, 
-#                         to_addrs="lucas.fedrigo@icloud.com", 
-#                         msg="Subject:Hello\n\nThis is the body of my email"
-#                         )
-    
+import smtplib
+import random
 import datetime as dt
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
+# year = now.year
+# month = now.month
+# day = now.day
 day_of_week = now.weekday()
 
-date_of_birth = dt.datetime(year=1991, month=8, day=17, hour=4, minute=20)
+def send_email():
 
-print(date_of_birth)
+    with open ("quotes.txt", "r") as file:
+        quotes = file.readlines()
+
+    random_quote = random.randint(0, 102)
+    daily_quote = quotes[random_quote]
+
+    my_email = "ilfedrigo.python.100days@gmail.com"
+    password = "ioqr rpwy jwdu xzig"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+                            from_addr=my_email, 
+                            to_addrs="lucas.fedrigo@icloud.com", 
+                            msg=f"Subject:Motivational Quote of the Day\n\n{daily_quote}"
+                            )
+
+if day_of_week == 6:
+    send_email()
+
+
+
+
+    
+
+
+
+# date_of_birth = dt.datetime(year=1991, month=8, day=17, hour=4, minute=20)
+
+# print(day_of_week)
